b337129664b620fb1d1ccea867bf0f632b3aad4b
Vitaliy <garrden01@yandex.ru>
2012-07-05 21:23:55 +0400

    happyticket

--- sdfsdfsd.pas
+++ sdfsdfsd.pas
@@ -1,17 +1,15 @@
-//Используй алгоритм!!! Нужно только изменить условие выхода из рекурсии.
+﻿//Используй алгоритм!!! Нужно только изменить условие выхода из рекурсии.
 
-const s = 9; //И что она делает? Определи константу matrix='0123456789'
-var
-Arr: array of integer;
-procedure happy(tic,p:integer; a: array of integer);
+const matrix='0123456789'; //И что она делает? Определи константу matrix='0123456789'
+procedure happy(tic,p:integer; m1:string);
 begin
-if tic = 6 then //Выход, когда длина скомпонованной строки равна 6 + "счастливое" условие.
+if tic = 6 then exit; //Выход, когда длина скомпонованной строки равна 6 + "счастливое" условие.
   begin
-    if (A[0]+A[1]+A[2])=(A[3]+A[4]+A[5]) then  //Используй функции StrToInt() и Copy()
+  m1:=copy(matrix,1,1);  
+    if m1+m2+m3=m4+m5+m6 then  //Используй функции StrToInt() и Copy()
     p:=p+1;
 writeln (p);
 end;
 end;
 begin
-  Arr:=new integer[s];
 end.
