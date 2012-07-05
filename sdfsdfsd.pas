a4d4046e8273d8054427eaa9574bd0d58dee8326
Vitaliy <garrden01@yandex.ru>
2012-07-05 16:52:12 +0400

    happyticket

---
+++ sdfsdfsd.pas
@@ -0,0 +1,15 @@
+const s = 9;
+var
+Arr: array of integer;
+procedure happy(tic,p:integer; a: array of integer);
+begin
+if tic = 6 then 
+  begin
+    if (A[0]+A[1]+A[2])=(A[3]+A[4]+A[5]) then 
+    p:=p+1;
+writeln (p);
+end;
+end;
+begin
+  Arr:=new integer[s];
+end.
